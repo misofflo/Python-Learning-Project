e4083b5470ba1e424ce70c51d6baf2860635c6ca
Florian Misof <misofflorian@gmail.com>
2025-11-19 15:36:35 +0100

    trying out variable data types

--- src/Main.py
+++ src/Main.py
@@ -1,1 +1,31 @@
+#this is a comment
+
+#this is a print statement
 print("I like Tiramisu!")
+
+#string variables
+first_name = "Florian"
+email_adress = "misofflorian@gmail.com"
+
+#int variables
+age = 23
+quantity = 5
+
+#float variables
+price = 10.99
+
+#boolean variables
+is_student = True
+is_rich = False
+
+#print formatted strings
+print(f"Hello, {first_name}!")
+print(f"You are {age} years old!")
+print(f"Your email adress is: {email_adress}")
+print(f"The price for Tiramisu is {price}€!")
+
+if is_student & is_rich:
+    print("You are a student!")
+    print("You are rich as well!")
+else:
+    print("You are not a student!")
